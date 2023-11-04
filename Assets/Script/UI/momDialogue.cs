028ed95bd73bdea9fa3bcf1afe5dc3cd591d4976
yeji-gim <77161592+yeji-gim@users.noreply.github.com>
2023-11-04 17:21:50 +0900

    UI
    UI

--- Assets/Script/UI/momDialogue.cs
+++ Assets/Script/UI/momDialogue.cs
@@ -5,65 +5,61 @@ using UnityEngine.EventSystems;
 
 public class momDialogue : MonoBehaviour
 {
-
+    public GameObject dialoguePanel;
     npcController npccontroller;
-
-    void Start()
+    Ray ray;
+    private void Start()
     {
-
+        dialoguePanel.gameObject.SetActive(false);
     }
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        if (Input.GetMouseButtonDown(0))
+        // 충돌한 객체의 태그가 "player"인지 확인
+        if (other.CompareTag("Player"))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            bool isPanelActive = UIManager.Instance.IsDialoguePromptActive();
+            if (!isPanelActive)
             {
-                Debug.Log("Gemtmousebutton");
-                OnInteractablenpccontroller(hit);
+                dialoguePanel.gameObject.SetActive(true);
             }
         }
     }
-
-    void OnInteractablenpccontroller(RaycastHit hit)
+    void StartMomDialogue()
     {
+        GameObject momObject = GameObject.FindGameObjectWithTag("momquest");
+        GameObject momObject2 = GameObject.FindGameObjectWithTag("mom");
+        QuestManager questManager = momObject.GetComponent<QuestManager>();
+        npccontroller = momObject2.GetComponent<npcController>();
 
-        Collider other = hit.collider;
-        if (hit.collider.CompareTag("mom"))
+        // 이하 대화 시작 로직 (이미 작성한 부분을 그대로 사용)
+        Debug.Log($"isquesting in momDialogue {questManager.getisQuesting()}");
+        if (DialogueManager.Instance != null)
         {
-            GameObject momObject = GameObject.FindGameObjectWithTag("momquest");
-            QuestManager questManager = momObject.GetComponent<QuestManager>();
-            npccontroller = hit.collider.GetComponent<npcController>();
-            Debug.Log($"isquesting in momDialogue {questManager.getisQuesting()}"); // 이 줄을 추가
-            if (npccontroller != null)
+            Debug.Log($"isquesting {questManager.getisQuesting()} && isComplete{questManager.getisCompleting()}");
+            if (questManager.getisCompleting() == false && questManager.getisQuesting() == false)
             {
-                if (DialogueManager.Instance != null)
-                {
-                    Debug.Log($"isquesting {questManager.getisQuesting()} && isComplete{questManager.getisCompleting()}");
-                    if (questManager.getisCompleting() == false&& questManager.getisQuesting() == false)
-                    {
-                        DialogueManager.Instance.StartDialogue(npccontroller.charcterData.dialogueLines);
-                    }
-                        
-                    else if (questManager.getisQuesting() == true && questManager.getisCompleting() == true)
-                    {
-                        DialogueManager.Instance.StartDialogue(npccontroller.charcterData.completedialogueLines);
-                    }
-                    // 퀘스트가 통과하지 않았을 때
-                    else if (questManager.getisQuesting() == true && questManager.getisCompleting() == false)
-                    {
-                        DialogueManager.Instance.StartDialogue(npccontroller.charcterData.noncompletedialogueLines);
-                    }
-                    Debug.Log("mom1");
-                }
-
+                DialogueManager.Instance.StartDialogue(npccontroller.charcterData.dialogueLines);
             }
-            else
+            else if (questManager.getisQuesting() == true && questManager.getisCompleting() == true)
             {
-                Debug.LogWarning("npccontroller ");
+                DialogueManager.Instance.StartDialogue(npccontroller.charcterData.completedialogueLines);
             }
+            else if (questManager.getisQuesting() == true && questManager.getisCompleting() == false)
+            {
+                DialogueManager.Instance.StartDialogue(npccontroller.charcterData.noncompletedialogueLines);
+            }
+            //Debug.Log("mom1");
         }
+    }
+    public void AcceptButton()
+    {
+        dialoguePanel.gameObject.SetActive(false);
+        StartMomDialogue();
+    }
+
+    public void RejectButton()
+    {
+        dialoguePanel.gameObject.SetActive(false);
     }
    
 }
