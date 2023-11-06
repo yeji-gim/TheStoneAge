c9574495b502ff00c259fd1ce27f6fc9266078f1
Zion2-6 <113418641+Zion2-6@users.noreply.github.com>
2023-11-06 16:03:42 +0900

    석기 제작

--- Assets/Script/Build Stone/StoneClick.cs
+++ Assets/Script/Build Stone/StoneClick.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class StoneClick : MonoBehaviour
 {
     public GameObject incompleteStone; // 미완성 돌 이미지
     public GameObject completeStone;   // 완성된 돌 이미지
+
+    public Slider clickCountSlider;    // 클릭 횟수를 표시할 슬라이더
+    public Text successText;           // 성공 텍스트
+    public Text completeText;          // 완성 텍스트
 
     public int clickCount = 0;
     public int maxCount;
@@ -13,22 +18,24 @@ public class StoneClick : MonoBehaviour
 
     private void Awake()
     {
-        InvokeRepeating("InstantiateButton", 1f,3f);
+        InvokeRepeating("InstantiateButton", 1f, 3f);
     }
 
     void Update()
     {
         // 클릭 횟수가 10번에 도달하면 완성된 돌 이미지 활성화
-        if (clickCount >= 10)
+        if (clickCount >= maxCount)
         {
             ActivateCompleteStone();
+            // InvokeRepeating 멈추기
+            CancelInvoke("InstantiateButton");
         }
     }
 
     void InstantiateButton()
     {
         Buttonobject.SetActive(false);
-        Buttonobject.GetComponent<RectTransform>().anchoredPosition = new Vector2(Random.Range(-600,600), Random.Range(-200, 200));
+        Buttonobject.GetComponent<RectTransform>().anchoredPosition = new Vector2(Random.Range(-600, 600), Random.Range(-200, 200));
         Buttonobject.SetActive(true);
     }
 
@@ -39,15 +46,51 @@ public class StoneClick : MonoBehaviour
 
         // 완성된 돌 이미지 활성화
         completeStone.SetActive(true);
+
+        // 완성 텍스트
+        completeText.gameObject.SetActive(true);
     }
+
+    // Success 버튼과 onClick으로 연결
     public void Success()
     {
         Buttonobject.SetActive(false);
         clickCount += 1;
+
+        // 성공 텍스트 활성화
+        successText.gameObject.SetActive(true);
+
+        // 슬라이더 업데이트
+        UpdateSlider();
+
+        // 성공 텍스트의 위치를 버튼의 위치로 조정
+        Vector2 buttonPosition = Buttonobject.GetComponent<RectTransform>().anchoredPosition;
+        successText.gameObject.GetComponent<RectTransform>().anchoredPosition = buttonPosition;
+
+        // 성공 텍스트 활성화
+        successText.gameObject.SetActive(true);
+
+        // 성공 텍스트 비활성화
+        StartCoroutine(DelayedSuccessTextDeactivation());
     }
 
+    // Fail 버튼과 onClick으로 연결
     public void Fail()
     {
         Buttonobject.SetActive(false);
+    }
+
+    // 슬라이더 업데이트
+    void UpdateSlider()
+    {
+        float normalizedValue = (float)clickCount / maxCount;
+        clickCountSlider.value = normalizedValue;
+    }
+
+    // 시간 지연 후 성공 텍스트 비활성화
+    IEnumerator DelayedSuccessTextDeactivation()
+    {
+        yield return new WaitForSeconds(1.0f); // 원하는 시간을 초 단위로 설정
+        successText.gameObject.SetActive(false);
     }
 }
