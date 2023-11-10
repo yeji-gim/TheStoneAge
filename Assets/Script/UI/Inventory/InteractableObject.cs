88170dcb2909c6362d278a08d72eb7e072eea187
yeji-gim <77161592+yeji-gim@users.noreply.github.com>
2023-11-10 15:26:44 +0900

    Item
    Item

--- Assets/Script/UI/Inventory/InteractableObject.cs
+++ Assets/Script/UI/Inventory/InteractableObject.cs
@@ -6,13 +6,13 @@ public class InteractableObject : MonoBehaviour
 {
     public ItemData item;
 
-    public void GetItem()
+    public void oldItem()
     {
-        bool isPanelActive = UIManager.Instance.IsInventoryPanelActive();
+        /*bool isPanelActive = UIManager.Instance.IsInventoryPanelActive();
         if (!isPanelActive)
         {
             ItemSlotData[] items = InventoryManager.Instance.GetInventorySlots(InventorySlot.InventoryType.Item);
-            bool addedToExistingStack = false;
+ 
 
             for (int i = 0; i < items.Length; i++)
             {
@@ -20,28 +20,36 @@ public class InteractableObject : MonoBehaviour
                 {
                     // 같은 아이템이 이미 있으면 수량을 추가
                     items[i].AddQuantity();
-                    addedToExistingStack = true;
                     break;
                 }
             }
 
-            // 같은 아이템이 없어서 새 슬롯에 추가하는 경우
-            if (!addedToExistingStack)
+            UIManager.Instance.RenderInventory();
+        */
+            Destroy(gameObject);
+        
+    }
+    public void newItem()
+    {
+        bool isPanelActive = UIManager.Instance.IsInventoryPanelActive();
+        if (!isPanelActive)
+        {
+            ItemSlotData[] items = InventoryManager.Instance.GetInventorySlots(InventorySlot.InventoryType.Item);
+
+
+            for (int i = 0; i < items.Length; i++)
             {
-                for (int i = 0; i < items.Length; i++)
+                if (items[i].itemData == null)
                 {
-                    if (items[i].itemData == null)
-                    {
-                        items[i].itemData = item;
-                        item = null;
-                        break;
-                    }
+                    items[i].itemData = item;
+                    item = null;
+                    break;
                 }
             }
-
-            // 인벤토리를 다시 렌더링하고 현재 게임 오브젝트를 제거합니다.
-            UIManager.Instance.RenderInventory();
-            Destroy(gameObject);
         }
+        // 인벤토리를 다시 렌더링하고 현재 게임 오브젝트를 제거합니다.
+        UIManager.Instance.RenderInventory();
+
+        Destroy(gameObject);
     }
 }
