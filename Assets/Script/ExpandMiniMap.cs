d07b5e971ccd5f3ff76be8b2afcd5d672fed92b5
sein0709 <psein0709@gmail.com>
2023-11-13 16:21:40 +0900

    곰 동굴 입구 추가
    곰 동굴 입구 추가

--- Assets/Script/ExpandMiniMap.cs
+++ Assets/Script/ExpandMiniMap.cs
@@ -6,20 +6,20 @@ using UnityEngine.UI;
 public class ExpandMiniMap : MonoBehaviour
 {
     private GameObject cam;
-    public Camera mapCamera;
+    // public Camera mapCamera;
     public float big = 30.0f;
     public Slider mapZoom;
 
     void Start()
     {
         cam = GameObject.Find("MiniMapCamera");
-        mapCamera = cam.GetComponent<Camera>();
+        // mapCamera = cam.GetComponent<Camera>();
     }
 
     void Update()
     {
         mapCamera.orthographicSize = big;
-        mapCamera.orthographicSize = mapZoom.value;
+        // mapCamera.orthographicSize = mapZoom.value;
 
     }
 }
