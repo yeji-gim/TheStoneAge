d5f4326fc919b51dbbea4974b3e74eb8557f307e
yeji-gim <77161592+yeji-gim@users.noreply.github.com>
2023-11-23 12:21:42 +0900

    quest
    quest

--- Assets/Script/UI/SoundManager.cs
+++ Assets/Script/UI/SoundManager.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public AudioSource bgm;
-
+    public AudioSource btnsource;
     private void Start()
     {
         bgm.Play();
     }
+
+    public void SetMusicVolume(float volume)
+    {
+        bgm.volume = volume;
+    }
+
+    public void StartButton()
+    {
+        btnsource.Play();
+    }
+
 }
